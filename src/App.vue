b99befd47458ca5abfce75d5a6bd2928a9edf8d2
DayanaMukan <dayyanaa.07@gmail.com>
2023-09-19 19:15:46 +0600

    <hw with footer>

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
 import NavbarComponet from './components/layouts/NavbarComponet.vue'
 import MyPanel from './components/MyPanel.vue'
 import MyFooter from './components/MyFooter.vue'
+import MyNavbar from './components/MyNavbar.vue'
 import {RouterView} from 'vue-router'
 </script>
 
 <template>
   <NavbarComponet />
+  <MyNavbar />
   <MyPanel />
   <MyFooter />
   <router-view/>
